24821b405e007f099cbe27fd483948db5c89e94c
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-11-13 14:51:49 +0900

    -cicd 조정

--- Learner/src/test/java/edu/example/learner/LearnerApplicationTests.java
+++ Learner/src/test/java/edu/example/learner/LearnerApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.example.learner;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LearnerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package edu.example.learner;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LearnerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
